288d3a184c55c1bb8adbd90125e3ee486db00472
chextopher <chrsjdevops@gmail.com>
2022-08-01 02:24:44 -0700

    Second commit of Simple1.java

--- Simple1.java
+++ Simple1.java
@@ -1,6 +1,6 @@
 class Simple{
 	public static void main(string args[]) {
-	System.out.println("Hello World!!");
+	System.out.println("Hello Edureka, Welcome to Git Online Training!");
 
         } 
 }
